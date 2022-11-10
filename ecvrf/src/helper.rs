851d970a573c1603ff16be0b3925b22efacaea9b
Chiro Hiro <chiro8x@gmail.com>
2022-11-10 17:06:58 +0700

    add key and randomness helper

--- ecvrf/src/helper.rs
+++ ecvrf/src/helper.rs
@@ -1,6 +1,20 @@
-use libsecp256k1::curve::{Affine, ECMultContext, ECMultGenContext, Field, Jacobian, Scalar};
+use libsecp256k1::{
+    curve::{Affine, ECMultContext, ECMultGenContext, Field, Jacobian, Scalar},
+    util::{FULL_PUBLIC_KEY_SIZE, SECRET_KEY_SIZE},
+    PublicKey, SecretKey,
+};
 use rand::{thread_rng, RngCore};
 use tiny_keccak::{Hasher, Keccak};
+
+pub struct KeyPair {
+    pub public_key: PublicKey,
+    pub secret_key: SecretKey,
+}
+
+pub struct RawKeyPair {
+    pub public_key: [u8; FULL_PUBLIC_KEY_SIZE],
+    pub secret_key: [u8; SECRET_KEY_SIZE],
+}
 
 // Perform multiplication between a point and a value: a*P
 pub fn ecmult(context: &ECMultContext, a: &Affine, na: &Scalar) -> Affine {
@@ -51,6 +65,34 @@ pub fn randomize() -> Scalar {
     let mut result = Scalar::default();
     result.set_b32(&random_bytes).unwrap_u8();
     result
+}
+
+// Generate a new key pair
+pub fn generate_keypair() -> KeyPair {
+    let mut rng = thread_rng();
+    let secret_key = SecretKey::random(&mut rng);
+    let public_key = PublicKey::from_secret_key(&secret_key);
+    KeyPair {
+        public_key,
+        secret_key,
+    }
+}
+
+pub fn generate_raw_keypair() -> RawKeyPair {
+    let mut rng = thread_rng();
+    let secret = SecretKey::random(&mut rng);
+    let secret_key = secret.serialize();
+    let public_key = PublicKey::from_secret_key(&secret).serialize();
+    RawKeyPair {
+        public_key,
+        secret_key,
+    }
+}
+
+// Random bytes
+pub fn random_bytes(buf: &mut [u8]) {
+    let mut rng = thread_rng();
+    rng.fill_bytes(buf);
 }
 
 // Normalize a Scalar
