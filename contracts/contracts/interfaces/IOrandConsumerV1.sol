ff2c240699109b1fefc65286b0d15e5e66270aa2
Chiro Hiro <chiro8x@gmail.com>
2023-08-06 13:54:49 +0700

    Move smart contracts to smart contracts repo

--- contracts/contracts/interfaces/IOrandConsumerV1.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.0;
-
-error InvalidProvider();
-
-interface IOrandConsumerV1 {
-  // Consume the verifiable randomness from provider
-  function consumeRandomness(uint256 randomness) external returns (bool);
-}
--- contracts/helpers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './const';
-export * from './singleton';
-export * from './deployer';
-export * from './functions';
