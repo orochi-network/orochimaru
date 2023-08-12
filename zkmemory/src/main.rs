3798a16a0447d58850ecf9a0de598ac43c3c7a78
unknown <caothangtrinh2002@gmail.com>
2023-08-12 15:24:48 +0700

    Add write method and create Value256 struct

--- zkmemory/src/main.rs
+++ zkmemory/src/main.rs
@@ -1,13 +1,12 @@
-use std::os::raw;
+//use std::os::raw;
 
-use zkmemory::memory::{Address256, Value256, GenericMemory, RawMemory, Uint256};
+use zkmemory::memory::{Address256, GenericMemory, RawMemory, Value256};
 // type inference lets us omit an explicit type signature (which
 // would be `RBTree<&str, &str>` in this example).
 
 fn main() {
     let mut raw_mem = RawMemory::<Address256, Value256>::new(256);
-
-    //println!("{:?}", raw_mem.cell_size());
+    println!("{:#016x?}", raw_mem.cell_size());
     // @note: Uint256 is big endian
     // raw_mem.write(
     //     Address256::from(0),
@@ -16,39 +15,41 @@ fn main() {
 
     // Write between 2 cells
     raw_mem.write(
-        Address256::from(5),
-        Value256::from(vec![0x0102030405060708u64, 0x090a0b0c0d0e0fu64, 0x1122334455667788u64, 0x99aabbccddeeffu64]),
+        Address256::from(60),
+        Value256::from(vec![0xaaaaaaaaaaaaaaaau64, 0xbbbbbbbbbbbbbbbbu64, 0xccccccccccccccccu64, 0xddddddddddddddddu64]),
     );
 
-    raw_mem.write(
-        Address256::from(64),
-        Value256::from(vec![0x0u64, 0x0u64, 0x9u64, 0x7u64]),
-    );
-
-    raw_mem.write(
-        Address256::from(256),
-        Value256::from(vec![0x0u64, 0x3u64, 0x5u64, 0x7u64]),
-    );
+    // println!("{}", type_of(raw_mem));
 
     // Iterative print to debug
-    for i in 0..64 {
+    for i in 0..10 {
         println!("{}, {:?}", i*32, raw_mem.read(Address256::from(i*32)));
     }
 
     // let mut raw_mem64 = RawMemory::<u64, u64>::new(64);
 
-    // raw_mem64.write(0, 1);
-    // raw_mem64.write(8, 2);
-    // raw_mem64.write(12, 0x199a993f);
+    // raw_mem64.write(
+    //     Address256::from(256),
+    //     Uint256::from_limbs(vec![0xffu64, 0x0u64, 0x0u64, 0u64]),
+    // );
 
-    // println!("{:#032x?}", raw_mem64.read(16));
-    // println!("{:#032x?}", raw_mem64.read(8));
+    // // Iterative print to debug
+    // for i in 0..64 {
+    //     println!("{}, {:?}", i*8, raw_mem64.read(Address256::from(i*8)));
+    // }
+
+    // // raw_mem64.write(0, 1);
+    // // raw_mem64.write(8, 2);
+    // // raw_mem64.write(12, 0x199a993f);
+
+    // // println!("{:#032x?}", raw_mem64.read(16));
+    // // println!("{:#032x?}", raw_mem64.read(8));
 
     // // @note: It's usual to little endian
-    // let a = 0x0102030405060708u64;
+    let a = 0x0102030405060708u64;
 
-    // println!("{:?}", a.to_ne_bytes());
-    // println!("{:?}", a.to_be_bytes());
-    // println!("{:?}", a.to_le_bytes());
+    println!("{:?}", a.to_ne_bytes());
+    println!("{:?}", a.to_be_bytes());
+    println!("{:?}", a.to_le_bytes());
 
 }
--- zkmemory/src/state_machine/memory.rs
+++ zkmemory/src/state_machine/memory.rs
@@ -1,6 +1,5 @@
 use core::ops::{Add, Div, Rem, Sub, Mul};
 use std::cell;
-
 use rbtree::RBTree;
 
 use revm_primitives::U256;
@@ -10,11 +9,36 @@ pub type Uint256 = U256;
 pub trait Address {
     fn is_zero(&self) -> bool;
     fn zero() -> Self;
+    fn to_u64(&self) -> u64;
 }
 pub trait Value {
     fn is_zero(&self) -> bool;
     fn zero() -> Self;
+    fn to_bytes_le(&self) -> Vec<u8>;
+    fn from_bytes(chunk: Vec<u8>) -> Self;
 }
+
+// pub trait Zeroable {
+//     fn is_zero(&self) -> bool;
+//     fn zero() -> Self;
+// }
+
+// pub trait Computable<K = Self>:
+//     Ord
+//     + From<u64>
+//     + Copy
+//     + PartialEq
+//     + Add<K, Output = K>
+//     + Sub<K, Output = K>
+//     + Rem<K, Output = K>
+//     + Div<K, Output = K>
+// {}
+
+// pub trait Address: Computable + Zeroable {}
+// pub trait Value: Computable + Zeroable {
+//     fn to_bytes_le(&self) -> Vec<u8>;
+//     fn from_bytes(chunk: Vec<u8>) -> Self;
+// }
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy)]
 pub struct Address256 {
@@ -23,26 +47,55 @@ pub struct Address256 {
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy)]
 pub struct Value256 {
-    value: U256,
+    inner: U256,
 }
 
-impl Address for u64 {
+// A function converting Vec<u8> to Vec<u64>
+fn convert_to_u64(input: Vec<u8>) -> Vec<u64> {
+    let mut result = Vec::with_capacity(4);
+    let mut current_value: u64 = 0;
+    let mut shift = 0;
+
+    for &byte in &input {
+        current_value |= (byte as u64) << shift;
+        shift += 8;
+
+        if shift == 64 {
+            result.push(current_value);
+            current_value = 0;
+            shift = 0;
+        }
+    }
+
+    if shift > 0 {
+        result.push(current_value);
+    }
+
+    // Pad with zeros if needed
+    while result.len() < 4 {
+        result.push(0);
+    }
+
+    result
+}
+
+impl Value for Value256 {
     fn is_zero(&self) -> bool {
-        *self == 0
+        self.inner.eq(&U256::ZERO)
     }
 
     fn zero() -> Self {
-        0
-    }
-}
-
-impl Value for u64 {
-    fn is_zero(&self) -> bool {
-        *self == 0
+        Self { inner: U256::ZERO }
     }
 
-    fn zero() -> Self {
-        0
+    fn to_bytes_le(&self) -> Vec<u8> {
+        Vec::from(self.inner.as_le_bytes())
+    }
+
+    fn from_bytes(chunk: Vec<u8>) -> Self {
+        Self {
+            inner : U256::from_limbs(convert_to_u64(chunk).try_into().unwrap())
+        }
     }
 }
 
@@ -54,17 +107,12 @@ impl Address for Address256 {
     fn zero() -> Self {
         Self { addr: U256::ZERO }
     }
-}
 
-impl Value for Value256 {
-    fn is_zero(&self) -> bool {
-        self.value.eq(&U256::ZERO)
-    }
-
-    fn zero() -> Self {
-        Self { value: U256::ZERO }
+    fn to_u64(&self) -> u64 {
+        self.addr.as_limbs()[3] as u64
     }
 }
+
 
 impl From<u64> for Address256 {
     fn from(value: u64) -> Self {
@@ -74,21 +122,14 @@ impl From<u64> for Address256 {
     }
 }
 
-impl From<u64> for Value256 {
-    fn from(value: u64) -> Self {
+impl From<Vec<u64>> for Value256 {
+    fn from(value: Vec<u64>) -> Self {
         Self {
-            value: U256::from_limbs([value, 0, 0, 0]),
+            inner: U256::from_limbs([value[0], value[1], value[2], value[3]]),
         }
     }
 }
 
-impl From<Vec<u64>> for Value256 {
-    fn from(value: Vec<u64>) -> Self {
-        Self {
-            value: U256::from_limbs([value[0], value[1], value[2], value[3]]),
-        }
-    }
-}
 
 impl Sub for Address256 {
     type Output = Self;
@@ -96,16 +137,6 @@ impl Sub for Address256 {
     fn sub(self, rhs: Self) -> Self::Output {
         Self {
             addr: self.addr - rhs.addr,
-        }
-    }
-}
-
-impl Sub for Value256 {
-    type Output = Self;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        Self {
-            value: self.value - rhs.value,
         }
     }
 }
@@ -120,32 +151,12 @@ impl Add for Address256 {
     }
 }
 
-impl Add for Value256 {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Self {
-            value: self.value + rhs.value,
-        }
-    }
-}
-
 impl Div for Address256 {
     type Output = Self;
 
     fn div(self, rhs: Self) -> Self::Output {
         Self {
             addr: self.addr / rhs.addr,
-        }
-    }
-}
-
-impl Div for Value256 {
-    type Output = Self;
-
-    fn div(self, rhs: Self) -> Self::Output {
-        Self {
-            value: self.value / rhs.value,
         }
     }
 }
@@ -160,12 +171,22 @@ impl Rem for Address256 {
     }
 }
 
-impl Rem for Value256 {
+impl Add for Value256 {
     type Output = Self;
 
-    fn rem(self, rhs: Self) -> Self::Output {
+    fn add(self, rhs: Self) -> Self::Output {
         Self {
-            value: self.value % rhs.value,
+            inner: self.inner + rhs.inner,
+        }
+    }
+}
+
+impl Sub for Value256 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            inner: self.inner - rhs.inner,
         }
     }
 }
@@ -175,7 +196,7 @@ impl Mul for Value256 {
 
     fn mul(self, rhs: Self) -> Self::Output {
         Self {
-            value: self.value * rhs.value,
+            inner: self.inner + rhs.inner,
         }
     }
 }
@@ -186,17 +207,11 @@ impl From<Address256> for usize {
     }
 }
 
-impl From<Value256> for usize {
-    fn from(value: Value256) -> Self {
-        value.value.as_limbs()[3] as usize
-    }
-}
-
 pub trait GenericMemory<K, V> {
     fn new(word_size: u64) -> Self;
     fn compute_address(&self, address: K) -> Vec<K>;
     fn read(&self, address: K) -> Option<&V>;
-    fn write(&mut self, address: K, value: V);
+    fn write(&mut self, address: K, value: V) -> u64;
     fn cell_size(&self) -> K;
     fn len(&self) -> usize;
 }
@@ -223,14 +238,11 @@ where
     V: Value
         + Ord
         + From<Vec<u64>>
-        + From<u64>
         + Copy
         + PartialEq
         + Add<V, Output = V>
         + Sub<V, Output = V>
-        + Rem<V, Output = V>
         + Mul<V, Output = V>
-        + Div<V, Output = V>,
 {
     fn new(word_size: u64) -> Self {
         if word_size % 8 != 0 {
@@ -249,41 +261,57 @@ where
             self.memory_map.get(&address)
         } else {
             // Read on the middle of the cell
-            self.memory_map.get(&address)
+
+            // Attempt to read 2 cells, low and high, then combine and return
+            self.memory_map.get(&address);
+            let cell_low = address - remain;
+            let cell_high = cell_low + self.cell_size();
+            let chunk_low_cell = self.memory_map.get(&cell_low);
+            let chunk_high_cell = self.memory_map.get(&cell_high);
+            match (chunk_low_cell, chunk_high_cell) {
+                (Some(chunk_low), Some(chunk_high)) => {
+                    // returns a value referencing data owned by the current function
+                    let result = V::from(vec![0u64, 1u64, 2u64, 3u64]);
+                    Some(&result)
+                },
+
+                (Some(chunk_low), None) => {
+                    // returns a value referencing data owned by the current function
+                    let result = V::from(vec![0u64, 1u64, 2u64, 3u64]);
+                    Some(&result)
+                },
+
+                (None, Some(chunk_high)) => {
+                    // returns a value referencing data owned by the current function
+                    let result = V::from(vec![0u64, 1u64, 2u64, 3u64]);
+                    Some(&result)
+                },
+
+                (None, None) => None,
+            }
         }
     }
 
-    fn write(&mut self, address: K, value: V) {
+    fn write(&mut self, address: K, value: V) -> u64{
         let remain = address % self.cell_size();
         if remain.is_zero() {
             self.memory_map.insert(address, value);
+            return 0;
+
         } else {
+
+            let chunk = value.to_bytes_le();
             let cell_low = address - remain;
             let cell_high = cell_low + self.cell_size();
-            let address_high = address + self.cell_size();
-            let mut i = address_high;
-            let mut slice = V::from(1);
-            let mut offset = V::from(1);
-            let base: u64 = 2;
-            while i > cell_high {
-                i = i - K::from(1);
-                slice = slice * V::from(base.pow(8));
-            }
-            i = address;
-            while i < cell_high {
-                i = i + K::from(1);
-                offset = offset * V::from(base.pow(8));
-            }
-            let chunk_low_cell = value / slice;
-            let chunk_high_cell = (value % slice) * offset;
+            let offset = (cell_high - address).to_u64();
+            
+            let chunk_low_cell = V::from(convert_to_u64(Vec::from(&chunk[0..offset as usize])));
+            let chunk_high_cell = V::from(convert_to_u64(Vec::from(&chunk[offset as usize..self.cell_size().to_u64() as usize])));
 
-            // self.memory_map.insert(cell_low, one_v);
-            // self.memory_map.insert(cell_high, value);
-            // self.memory_map.insert(address, one_v);
-            // self.memory_map.insert(address_high, value);
             self.memory_map.insert(cell_low, chunk_low_cell);
             self.memory_map.insert(cell_high, chunk_high_cell);
-            //self.memory_map.insert(address + self.cell_size - remain, V::from(mod_chunk));
+
+            return 1;
         };
     }
 
