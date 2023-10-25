e58ac514f169ce484cc4c3ed593aa5c935d10b5c
Chiro Hiro <chiro8x@gmail.com>
2023-10-25 23:02:59 +0700

    Improve Base<S> make base easier to use

--- zkmemory/src/base.rs
+++ zkmemory/src/base.rs
@@ -1,17 +1,26 @@
 use core::fmt::{Debug, Display};
 use core::ops::{Add, Div, Mul, Rem, Sub};
 use core::usize;
-use ethnum::AsU256;
-pub use ethnum::U256;
+use ethnum::U256;
 
 /// Base trait for memory address and value
 pub trait Base<const S: usize = 0, T = Self>:
     Ord
     + Copy
     + PartialEq
-    + UsizeConvertible
+    + Eq
+    + Ord
+    + PartialOrd
     + Display
     + Debug
+    + From<i32>
+    + Into<i32>
+    + From<usize>
+    + Into<usize>
+    + From<u64>
+    + Into<u64>
+    + From<[u8; S]>
+    + Into<[u8; S]>
     + Add<T, Output = T>
     + Mul<T, Output = T>
     + Sub<T, Output = T>
@@ -30,92 +39,200 @@ pub trait Base<const S: usize = 0, T = Self>:
     fn is_zero(&self) -> bool;
     /// Get the zero value
     fn zero() -> Self;
-    /// Convert to big endian bytes
-    fn to_bytes(&self) -> [u8; S];
-    /// Convert from big endian bytes
-    fn from_bytes(bytes: [u8; S]) -> Self;
 }
 
 /// Convert from/to [usize](core::usize)
-pub trait UsizeConvertible {
+pub trait UIntConvertible {
     /// Convert from [usize](core::usize)
     fn from_usize(value: usize) -> Self;
     /// Convert to [usize](core::usize)
     fn to_usize(&self) -> usize;
 }
 
-impl UsizeConvertible for U256 {
-    fn from_usize(value: usize) -> Self {
-        value.as_u256()
-    }
+/// Uint256 is a wrapper of [U256](ethnum::U256) to implement [Base](crate::base::Base)
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct Uint<T>(T);
 
-    fn to_usize(&self) -> usize {
-        self.as_usize()
+impl<T: Display> Display for Uint<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        Display::fmt(&self.0, f)
     }
 }
 
-impl Base<32> for U256 {
-    const MAX: Self = U256::MAX;
+impl<T: Div<Output = T>> Div for Uint<T> {
+    type Output = Self;
 
-    const MIN: Self = U256::MIN;
-
-    const WORD_SIZE: Self = U256::new(32);
-
-    fn is_zero(&self) -> bool {
-        self.eq(&U256::ZERO)
+    fn div(self, rhs: Self) -> Self::Output {
+        Self(self.0 / rhs.0)
     }
+}
 
-    fn zero() -> Self {
-        U256::ZERO
+impl<T: Add<Output = T>> Add for Uint<T> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0)
     }
+}
 
-    fn to_bytes(&self) -> [u8; 32] {
-        self.to_be_bytes()
+impl<T: Sub<Output = T>> Sub for Uint<T> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self(self.0 - rhs.0)
     }
+}
 
-    fn from_bytes(bytes: [u8; 32]) -> Self {
-        Self::from_be_bytes(bytes)
+impl<T: Rem<Output = T>> Rem for Uint<T> {
+    type Output = Self;
+
+    fn rem(self, rhs: Self) -> Self::Output {
+        Self(self.0 % rhs.0)
+    }
+}
+
+impl<T: Mul<Output = T>> Mul for Uint<T> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self(self.0 * rhs.0)
     }
 }
 
 macro_rules! new_base {
-    ($primitive:ident, $byte_size: expr) => {
-        impl UsizeConvertible for $primitive {
-            fn from_usize(value: usize) -> Self {
-                value as $primitive
-            }
+    (U256, $byte_size: expr) => {
+        impl Base<32> for Uint<U256> {
+            const MAX: Self = Self(U256::MAX);
 
-            fn to_usize(&self) -> usize {
-                *self as usize
-            }
-        }
+            const MIN: Self = Self(U256::MIN);
 
-        impl Base<$byte_size> for $primitive {
-            const MAX: Self = $primitive::MAX;
-
-            const MIN: Self = $primitive::MIN;
-
-            const WORD_SIZE: Self = $byte_size as $primitive;
+            const WORD_SIZE: Self = Self(U256::new(32));
 
             fn is_zero(&self) -> bool {
-                *self == 0
+                self.0 == U256::ZERO
             }
 
             fn zero() -> Self {
-                0
+                Self(U256::ZERO)
+            }
+        }
+
+        impl From<i32> for Uint<U256> {
+            fn from(value: i32) -> Self {
+                Self(U256::new(value as u128))
+            }
+        }
+
+        impl From<usize> for Uint<U256> {
+            fn from(value: usize) -> Self {
+                Self(U256::new(value as u128))
+            }
+        }
+
+        impl From<u64> for Uint<U256> {
+            fn from(value: u64) -> Self {
+                Self(U256::new(value as u128))
+            }
+        }
+
+        impl Into<i32> for Uint<U256> {
+            fn into(self) -> i32 {
+                self.0.as_i32()
+            }
+        }
+
+        impl Into<usize> for Uint<U256> {
+            fn into(self) -> usize {
+                self.0.as_usize()
+            }
+        }
+
+        impl Into<u64> for Uint<U256> {
+            fn into(self) -> u64 {
+                self.0.as_u64()
+            }
+        }
+
+        impl Into<[u8; 32]> for Uint<U256> {
+            fn into(self) -> [u8; 32] {
+                self.0.to_be_bytes()
+            }
+        }
+
+        impl From<[u8; 32]> for Uint<U256> {
+            fn from(value: [u8; 32]) -> Self {
+                Self(U256::from_be_bytes(value))
+            }
+        }
+    };
+    ($primitive:ident, $byte_size: expr) => {
+        impl Base<$byte_size> for Uint<$primitive> {
+            const MAX: Self = Self($primitive::MAX);
+
+            const MIN: Self = Self($primitive::MIN);
+
+            const WORD_SIZE: Self = Self($byte_size as $primitive);
+
+            fn is_zero(&self) -> bool {
+                self.0 == 0
             }
 
-            fn to_bytes(&self) -> [u8; $byte_size] {
-                self.to_be_bytes()
+            fn zero() -> Self {
+                Self(0)
             }
+        }
 
-            fn from_bytes(bytes: [u8; $byte_size]) -> Self {
-                Self::from_be_bytes(bytes)
+        impl From<i32> for Uint<$primitive> {
+            fn from(value: i32) -> Self {
+                Self(value as $primitive)
+            }
+        }
+
+        impl From<usize> for Uint<$primitive> {
+            fn from(value: usize) -> Self {
+                Self(value as $primitive)
+            }
+        }
+
+        impl From<u64> for Uint<$primitive> {
+            fn from(value: u64) -> Self {
+                Self(value as $primitive)
+            }
+        }
+
+        impl Into<i32> for Uint<$primitive> {
+            fn into(self) -> i32 {
+                self.0 as i32
+            }
+        }
+
+        impl Into<usize> for Uint<$primitive> {
+            fn into(self) -> usize {
+                self.0 as usize
+            }
+        }
+
+        impl Into<u64> for Uint<$primitive> {
+            fn into(self) -> u64 {
+                self.0 as u64
+            }
+        }
+
+        impl Into<[u8; $byte_size]> for Uint<$primitive> {
+            fn into(self) -> [u8; $byte_size] {
+                self.0.to_be_bytes()
+            }
+        }
+
+        impl From<[u8; $byte_size]> for Uint<$primitive> {
+            fn from(value: [u8; $byte_size]) -> Self {
+                Self($primitive::from_be_bytes(value))
             }
         }
     };
 }
 
+new_base!(U256, 32);
 new_base!(u64, 8);
 new_base!(u32, 4);
 new_base!(u16, 2);
