a6cf39e4bb8ee444c74e8d70cf62fd3c7c8f55ee
unknown <caothangtrinh2002@gmail.com>
2023-10-30 14:06:34 +0700

    Adjust example

--- zkmemory/examples/256bits-machine.rs
+++ zkmemory/examples/256bits-machine.rs
@@ -8,14 +8,17 @@ fn main() {
     // Define the desired machine configuration
     let mut machine = StateMachine::<B256, B256, 32, 32>::new(DefaultConfig::default());
 
+    // Get the base address of the memory section
+    let base = machine.base_address();
+
     // Define your desired program
     let program = vec![
-        Instruction::Write(B256::from(16), B256::from(1025)),
-        Instruction::Load(machine.r0, B256::from(16)),
+        Instruction::Write(base + B256::from(16), B256::from(1025)),
+        Instruction::Load(machine.r0, base + B256::from(16)),
         Instruction::Push(B256::from(3735013596u64)),
         Instruction::Swap(machine.r1),
         Instruction::Add(machine.r0, machine.r1),
-        Instruction::Save(B256::from(24), machine.r0),
+        Instruction::Save(base + B256::from(24), machine.r0),
     ];
 
     // Execute the program
@@ -27,8 +30,6 @@ fn main() {
     for x in machine.trace().into_iter() {
         println!("{:?}", x);
     }
-
-    machine.exec(&Instruction::Read(B256::from(32776)));
 
 
 }
