a1fd08bfd531cec1757238be737a02c439176225
unknown <caothangtrinh2002@gmail.com>
2023-11-18 14:44:46 +0700

    Add conversion methods tests

--- zkmemory/src/commitment/kzg.rs
+++ zkmemory/src/commitment/kzg.rs
@@ -4,11 +4,7 @@ use ff::{Field, PrimeField};
 use group::Curve;
 use rand_core::OsRng;
 
-use crate::{
-    base::Base,
-    machine::TraceRecord,
-    machine::MemoryInstruction,
-};
+use crate::{base::Base, machine::MemoryInstruction, machine::TraceRecord};
 use halo2_proofs::{
     arithmetic::lagrange_interpolate,
     poly::{
@@ -30,7 +26,6 @@ where
     kzg_params: ParamsKZG<Bn256>,
     // Domain used for creating polynomials
     domain: EvaluationDomain<Fr>,
-
     // Phantom data for K and V
     _marker_k: PhantomData<K>,
     _marker_v: PhantomData<V>,
@@ -43,7 +38,7 @@ where
 {
     /// Initialize KZG parameters
     /// K = 3 since we need the poly degree to be 2^3 = 8
-    pub fn setup() -> Self {
+    pub fn new() -> Self {
         const K: u32 = 3;
         Self {
             kzg_params: ParamsKZG::<Bn256>::new(K),
@@ -55,7 +50,10 @@ where
 
     /// Commit a trace record in an execution trace
     pub fn commit(&mut self, trace: TraceRecord<K, V, S, T>) -> G1Affine {
+        // Convert trace record into polynomial
         let poly = self.poly_from_trace(trace);
+
+        // Commit the polynomial using Halo2's code
         let alpha = Blind(Fr::random(OsRng));
         let commitment = self.kzg_params.commit(&poly, alpha);
         commitment.to_affine()
@@ -110,6 +108,8 @@ where
             current_point *= Fr::MULTIPLICATIVE_GENERATOR;
             points_arr[i] = current_point;
         }
+
+        // Use Lagrange interpolation
         self.domain.coeff_from_vec(lagrange_interpolate(
             points_arr.as_slice(),
             evals.as_slice(),
@@ -117,16 +117,61 @@ where
     }
 
     // Convert 32 bytes number to field elements
+    // This is made compatible with the Fr endianess
     fn be_bytes_to_field(&self, bytes: &mut [u8]) -> Fr {
         bytes.reverse();
         let b = bytes.as_ref();
         let inner = [0, 8, 16, 24].map(|i| u64::from_le_bytes(b[i..i + 8].try_into().unwrap()));
-        Fr::from_raw(inner)
+        let result = Fr::from_raw(inner);
+        result
     }
 }
 
 #[cfg(test)]
 mod test {
 
-    
+    use super::*;
+    use crate::{base::B256, machine::AbstractTraceRecord};
+    use halo2_proofs::arithmetic::eval_polynomial;
+
+    #[test]
+    fn test_conversion_fr() {
+        let kzg_scheme = KZGMemoryCommitment::<B256, B256, 32, 32>::new();
+        let mut chunk = [0u8; 32];
+        for i in 0..32 {
+            chunk[i] = i as u8;
+        }
+
+        let fr = kzg_scheme.be_bytes_to_field(chunk.as_mut_slice());
+        let chunk_fr: [u8; 32] = fr.try_into().unwrap();
+
+        assert_eq!(chunk_fr, chunk);
+    }
+
+    #[test]
+    fn test_record_polynomial_conversion() {
+        let kzg_scheme = KZGMemoryCommitment::<B256, B256, 32, 32>::new();
+
+        // Initialize a trace record
+        let trace = TraceRecord::<B256, B256, 32, 32>::new(
+            1u64,
+            2u64,
+            MemoryInstruction::Read,
+            B256::zero(),
+            B256::from(100),
+        );
+
+        // Get the polynomial
+        let poly_trace = kzg_scheme.poly_from_trace(trace);
+
+        // Get only the evals, which is the trace record's elements converted to field elements
+        let poly_evals = kzg_scheme.trace_to_field(trace);
+
+        // Test each eval values
+        let mut base_index = Fr::ONE;
+        for i in 0..8 {
+            assert_eq!(eval_polynomial(&poly_trace, base_index), poly_evals[i]);
+            base_index *= Fr::MULTIPLICATIVE_GENERATOR;
+        }
+    }
 }
