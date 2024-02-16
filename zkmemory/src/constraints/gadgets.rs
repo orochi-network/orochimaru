866297b502e9b1db71933290b793d09040bbe3f7
phamnhatminh1292001 <66765903+phamnhatminh1292001@users.noreply.github.com>
2024-02-16 16:58:55 +0700

    Add more gates in configure method
    This PR continues implementing the gates for the lexcographic chip

--- zkmemory/src/constraints/gadgets.rs
+++ zkmemory/src/constraints/gadgets.rs
@@ -1,1 +1,48 @@
+extern crate alloc;
+use alloc::vec;
+use alloc::vec::Vec;
+use ff::Field;
+use halo2_proofs::{
+    circuit::{Layouter, Value},
+    plonk::{Column, ConstraintSystem, Error, Expression, Fixed, VirtualCells},
+    poly::Rotation,
+};
 
+/// Lookup table for max n bits range check
+#[derive(Clone, Copy, Debug)]
+pub struct UTable<const N_BITS: usize> {
+    col: Column<Fixed>,
+}
+
+impl<const N_BITS: usize> UTable<N_BITS> {
+    /// Construct the UTable.
+    pub fn construct<F: Field>(meta: &mut ConstraintSystem<F>) -> Self {
+        Self {
+            col: meta.fixed_column(),
+        }
+    }
+
+    /// Load the `UTable` for range check
+    pub fn load<F: Field>(&self, layouter: &mut impl Layouter<F>) -> Result<(), Error> {
+        layouter.assign_region(
+            || "loading column",
+            |mut region| {
+                for i in 0..(1 << N_BITS) {
+                    region.assign_fixed(
+                        || "assigning values to column",
+                        self.col,
+                        i,
+                        || Value::known(F::from(i as u64)),
+                    )?;
+                }
+                Ok(())
+            },
+        )?;
+        Ok(())
+    }
+
+    /// Return the list of expressions used to define the table
+    pub fn table_exprs<F: Field>(&self, meta: &mut VirtualCells<'_, F>) -> Vec<Expression<F>> {
+        vec![meta.query_fixed(self.col, Rotation::cur())]
+    }
+}
--- zkmemory/src/constraints/lexicographic_ordering.rs
+++ zkmemory/src/constraints/lexicographic_ordering.rs
@@ -1,13 +1,15 @@
 extern crate alloc;
 use crate::{base::Base, machine::TraceRecord};
-use alloc::{format, vec};
+use alloc::vec;
 use core::marker::PhantomData;
 use halo2_proofs::{
     arithmetic::Field,
-    circuit::{Chip, Region, Value},
-    plonk::{Advice, Column, ConstraintSystem, Error, Expression, Fixed, Instance, Selector},
+    circuit::Chip,
+    plonk::{Advice, Column, ConstraintSystem, Expression, Selector, VirtualCells},
     poly::Rotation,
 };
+
+use super::gadgets::UTable;
 // We use this chip to show that the rows of the memory trace table are sorted
 // in a lexicographic order (by address, time log, opcode).
 
@@ -15,15 +17,15 @@ use halo2_proofs::{
 
 // Advice columns:
 // 1. first_different_limb: the first index where limb differs
-// 2. limb_difference: the difference between the limbs at first_different_limb.
+// 2. difference: the difference between the limbs at first_different_limb.
 // By definition, this value is non-zero
-// 3. limb_difference_inverse: the inverse of limb_difference.
+// 3. difference_inverse: the inverse of difference.
 
 // Constraints:
-// 1. limb_difference must be non-zero.
+// 1. difference must be non-zero.
 // 2. all the pairwise limb differences before the first_different_limb is
 // zero, due to the definition of first_different_limb.
-// 3. limb_difference equals the difference of the limbs at first_different_limb.
+// 3. difference equals the difference of the limbs at first_different_limb.
 
 #[derive(Clone, Copy, Debug)]
 // define the columns for the constraint
@@ -34,6 +36,8 @@ pub struct LexicographicConfig {
     difference_inverse: Column<Advice>,
     // selector for the inversion constraint
     sel_inv: Selector,
+    // selector for constraining all limbs before first difference are zero
+    sel_zero: Selector,
 }
 
 struct LexicographicChip<F: Field, K, V, const S: usize, const T: usize>
@@ -45,6 +49,8 @@ where
     _marker: PhantomData<(K, V, F)>,
 }
 
+// implement the chip trait
+// these functions are not needed
 impl<F: Field, K, V, const S: usize, const T: usize> Chip<F> for LexicographicChip<F, K, V, S, T>
 where
     K: Base<S>,
@@ -61,6 +67,9 @@ where
     }
 }
 
+// implement the configure method for selecting gates
+// we have the gates for checking inverse, lookup and checking that
+// all values before first_difference_limb are equal to zero
 impl<F: Field, K, V, const S: usize, const T: usize> LexicographicChip<F, K, V, S, T>
 where
     K: Base<S>,
@@ -68,25 +77,80 @@ where
 {
     fn configure(
         meta: &mut ConstraintSystem<F>,
-        advice: [Column<Advice>; 2],
         trace: TraceRecord<K, V, S, T>,
-        instance: Column<Instance>,
-        constant: Column<Fixed>,
-    ) {
+        u16_table: UTable<16>,
+        alpha_power: [Expression<F>; 31],
+    ) -> <Self as Chip<F>>::Config {
         let one = Expression::Constant(F::ONE);
-        meta.enable_equality(instance);
-        meta.enable_constant(constant);
-        for column in &advice {
-            meta.enable_equality(*column);
-        }
+        //   meta.enable_equality(u16_table);
 
+        let difference = meta.advice_column();
+        let difference_inverse = meta.advice_column();
         let sel_inv = meta.selector();
+        let sel_zero = meta.selector();
 
+        // inversion gate
         meta.create_gate("difference is non-zero", |meta| {
             let sel_inv = meta.query_selector(sel_inv);
-            let difference = meta.query_advice(advice[0], Rotation::cur());
-            let difference_inverse = meta.query_advice(advice[1], Rotation::cur());
+            let difference = meta.query_advice(difference, Rotation::cur());
+            let difference_inverse = meta.query_advice(difference_inverse, Rotation::cur());
             vec![sel_inv * (difference * difference_inverse - one)]
         });
+
+        // limbs before first differences are zero
+        meta.create_gate("limbs before first differences are zero", |meta| {
+            let sel_zero = meta.query_selector(sel_zero);
+            // TODO: implement Queries struct first, then use the constraints on this
+            let cur = Queries::new(meta, trace, Rotation::cur());
+            let prev = Queries::new(meta, trace, Rotation::prev());
+            vec![]
+        });
+
+        // lookup gate
+        meta.lookup_any("difference is in u16", |meta| {
+            let difference = meta.query_advice(difference, Rotation::cur());
+            let col = u16_table.table_exprs(meta);
+            vec![difference]
+                .into_iter()
+                .zip_eq(col)
+                .map(|(difference, col)| (difference, col))
+                .collect()
+        });
+
+        LexicographicConfig {
+            difference,
+            difference_inverse,
+            sel_inv,
+            sel_zero,
+        }
+    }
+}
+
+// convert a trace record into a list of element having the form of Expression<F>
+struct Queries<F: Field, K, V, const S: usize, const T: usize>
+where
+    K: Base<S>,
+    V: Base<T>,
+{
+    address: [Expression<F>; 32],  //64 bits
+    time_log: [Expression<F>; 32], //64 bits
+    instruction: Expression<F>,    // 0 or 1
+    phantom_data: PhantomData<(K, V, F)>,
+}
+
+impl<F: Field, K, V, const S: usize, const T: usize> Queries<F, K, V, S, T>
+where
+    K: Base<S>,
+    V: Base<T>,
+{
+    fn new(
+        meta: &mut VirtualCells<'_, F>,
+        trace: TraceRecord<K, V, S, T>,
+        rotation: Rotation,
+    ) -> Self {
+        let mut query_advice = |column| meta.query_advice(column, rotation);
+        Self {
+            phantom_data: PhantomData,
+        }
     }
 }
