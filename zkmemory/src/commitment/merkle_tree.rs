b4643441d7ac3c3bb11b15a7e0f07e1caa59cd0e
phamnhatminh1292001 <66765903+phamnhatminh1292001@users.noreply.github.com>
2024-03-26 17:07:32 +0700

    Reconfigure poseidon config

--- zkmemory/src/commitment/merkle_tree.rs
+++ zkmemory/src/commitment/merkle_tree.rs
@@ -1,209 +1,154 @@
 // this is based on the implementation of https://github.com/DrPeterVanNostrand/halo2-merkle
 extern crate alloc;
 use alloc::{format, vec, vec::Vec};
-use core::marker::PhantomData;
+use core::{fmt::Debug, marker::PhantomData};
 use ff::{Field, PrimeField};
 use halo2_proofs::{
-    circuit::{Layouter, Region, SimpleFloorPlanner, Value},
-    plonk::{Advice, Circuit, Column, ConstraintSystem, Error, Fixed, Instance, Selector},
+    circuit::{AssignedCell, Cell, Layouter, Region, SimpleFloorPlanner, Value},
+    plonk::{
+        Advice, Circuit, Column, ConstraintSystem, Constraints, Error, Expression, Fixed, Instance,
+        Selector,
+    },
     poly::Rotation,
 };
 
-#[derive(Clone, Debug)]
-struct PoseidonChipConfig {
-    a_col: Column<Advice>,
-    sbox_out_col: Column<Advice>,
-    mds_out_col: Column<Advice>,
-    pre_key_col: Column<Fixed>,
-    post_key_col: Column<Fixed>,
-    mds_cols: Vec<Column<Fixed>>,
-    s_sbox_pre_post: Selector,
-    s_sbox_post: Selector,
-    s_sbox_no_add: Selector,
-    s_mds: Vec<Selector>,
-    perm_output_to_input: Permutation,
-    perm_output_to_sbox_output: Permutation,
-}
-
-impl Chip<Fp> for PoseidonChip {
-    type Config = PoseidonChipConfig;
-    type Loaded = ();
-
-    fn config(&self) -> &Self::Config {
-        &self.config
-    }
-
-    fn loaded(&self) -> &Self::Loaded {
-        &()
-    }
-}
-
-impl PoseidonChip {
-    fn new(config: PoseidonChipConfig) -> Self {
-        PoseidonChip { config }
-    }
-
-    fn configure(
-        meta: &mut ConstraintSystem<Fp>,
-        digest_col: Column<Advice>,
-    ) -> PoseidonChipConfig {
-        let a_col = meta.advice_column();
-        let sbox_out_col = meta.advice_column();
-        let mds_out_col = digest_col;
-        let pre_key_col = meta.fixed_column();
-        let post_key_col = meta.fixed_column();
-        let mds_cols = vec![
-            meta.fixed_column(),
-            meta.fixed_column(),
-            meta.fixed_column(),
-        ];
-
-        let s_sbox_pre_post = meta.selector();
-        let s_sbox_post = meta.selector();
-        let s_sbox_no_add = meta.selector();
-        let s_mds = vec![meta.selector(), meta.selector(), meta.selector()];
-
-        meta.create_gate("s_sbox_pre_post", |meta| {
-            let a = meta.query_advice(a_col, Rotation::cur());
-            let pre_key = meta.query_fixed(pre_key_col, Rotation::cur());
-            let post_key = meta.query_fixed(post_key_col, Rotation::cur());
-            let sbox_out = meta.query_advice(sbox_out_col, Rotation::cur());
-            let s_sbox_pre_post = meta.query_selector(s_sbox_pre_post, Rotation::cur());
-            // (a + pre_key)^5 + post_key = out
-            let a_plus_pre = a + pre_key;
-            s_sbox_pre_post
-                * (a_plus_pre.clone()
-                    * a_plus_pre.clone()
-                    * a_plus_pre.clone()
-                    * a_plus_pre.clone()
-                    * a_plus_pre
-                    + post_key
-                    - sbox_out)
-        });
-
-        meta.create_gate("s_sbox_post", |meta| {
-            let a = meta.query_advice(a_col, Rotation::cur());
-            let post_key = meta.query_fixed(post_key_col, Rotation::cur());
-            let sbox_out = meta.query_advice(sbox_out_col, Rotation::cur());
-            let s_sbox_post = meta.query_selector(s_sbox_post, Rotation::cur());
-            // a^5 + post_key = b
-            s_sbox_post * (a.clone() * a.clone() * a.clone() * a.clone() * a + post_key - sbox_out)
-        });
-
-        meta.create_gate("s_sbox_no_add", |meta| {
-            let a = meta.query_advice(a_col, Rotation::cur());
-            let sbox_out = meta.query_advice(sbox_out_col, Rotation::cur());
-            let s_sbox_no_add = meta.query_selector(s_sbox_no_add, Rotation::cur());
-            // a^5 = b
-            s_sbox_no_add * (a.clone() * a.clone() * a.clone() * a.clone() * a - sbox_out)
-        });
-
-        // Calculates the dot product of the sbox outputs with column `0` of the MDS matrix. Note
-        // that `s_mds_0` is enabled in the first MDS row.
-        meta.create_gate("s_mds_0", |meta| {
-            let sbox_out_0 = meta.query_advice(sbox_out_col, Rotation::cur());
-            let sbox_out_1 = meta.query_advice(sbox_out_col, Rotation::next());
-            let sbox_out_2 = meta.query_advice(sbox_out_col, Rotation(2));
-            let mds_out_0 = meta.query_advice(mds_out_col, Rotation::cur());
-            let s_mds_0 = meta.query_selector(s_mds[0], Rotation::cur());
-
-            // The first MDS column.
-            let m_0 = meta.query_fixed(mds_cols[0], Rotation::cur());
-            let m_1 = meta.query_fixed(mds_cols[0], Rotation::next());
-            let m_2 = meta.query_fixed(mds_cols[0], Rotation(2));
-
-            // Dot product of sbox outputs with the first MDS column.
-            s_mds_0 * (sbox_out_0 * m_0 + sbox_out_1 * m_1 + sbox_out_2 * m_2 - mds_out_0)
-        });
-
-        // Calculates the dot product of the sbox outputs with column `1` of the MDS matrix. Note
-        // that `s_mds_1` is enabled in the second MDS row.
-        meta.create_gate("s_mds_1", |meta| {
-            let sbox_out_0 = meta.query_advice(sbox_out_col, Rotation::prev());
-            let sbox_out_1 = meta.query_advice(sbox_out_col, Rotation::cur());
-            let sbox_out_2 = meta.query_advice(sbox_out_col, Rotation::next());
-            let mds_out_1 = meta.query_advice(mds_out_col, Rotation::cur());
-            let s_mds_1 = meta.query_selector(s_mds[1], Rotation::cur());
-
-            // The second MDS column.
-            let m_0 = meta.query_fixed(mds_cols[1], Rotation::prev());
-            let m_1 = meta.query_fixed(mds_cols[1], Rotation::cur());
-            let m_2 = meta.query_fixed(mds_cols[1], Rotation::next());
-
-            // Dot product of the sbox outputs with the second MDS column.
-            s_mds_1 * (sbox_out_0 * m_0 + sbox_out_1 * m_1 + sbox_out_2 * m_2 - mds_out_1)
-        });
-
-        // Calculates the dot product of the sbox outputs with column `2` of the MDS matrix. Note
-        // that `s_mds_2` is enabled in the third MDS row.
-        meta.create_gate("s_mds_2", |meta| {
-            let sbox_out_0 = meta.query_advice(sbox_out_col, Rotation(-2));
-            let sbox_out_1 = meta.query_advice(sbox_out_col, Rotation::prev());
-            let sbox_out_2 = meta.query_advice(sbox_out_col, Rotation::cur());
-            let mds_out_2 = meta.query_advice(mds_out_col, Rotation::cur());
-            let s_mds_2 = meta.query_selector(s_mds[2]);
-
-            // The third MDS column.
-            let m_0 = meta.query_fixed(mds_cols[2], Rotation(-2));
-            let m_1 = meta.query_fixed(mds_cols[2], Rotation::prev());
-            let m_2 = meta.query_fixed(mds_cols[2], Rotation::cur());
-
-            // Dot product of the sbox outputs with the third MDS column.
-            s_mds_2 * (sbox_out_0 * m_0 + sbox_out_1 * m_1 + sbox_out_2 * m_2 - mds_out_2)
-        });
-
-        // Copies a round's MDS output into the next round's state.
-        let perm_output_to_input = Permutation::new(meta, &[mds_out_col.into(), a_col.into()]);
-
-        // Copies a round's MDS output into the next round's sbox output.
-        let perm_output_to_sbox_output =
-            Permutation::new(meta, &[mds_out_col.into(), sbox_out_col.into()]);
-
-        PoseidonChipConfig {
-            a_col,
-            sbox_out_col,
-            mds_out_col,
-            pre_key_col,
-            post_key_col,
-            mds_cols,
-            s_sbox_pre_post,
-            s_sbox_post,
-            s_sbox_no_add,
-            s_mds,
-            perm_output_to_input,
-            perm_output_to_sbox_output,
-        }
-    }
-}
-
 #[derive(Clone, Copy, Debug)]
 /// Poseidon config
-pub struct PoseidonConfig<F: Field + PrimeField> {
-    path: Column<Advice>,
-    sibling: Column<Advice>,
+pub struct PoseidonConfig<F: Field + PrimeField, const W: usize, const R: usize> {
+    state: [Column<Advice>; W],
+    sbox: Column<Advice>,
+    rc_a: [Column<Fixed>; W],
+    rc_b: [Column<Fixed>; W],
+    s_full: Selector,
+    s_partial: Selector,
+    s_pad_and_add: Selector,
+    alpha: [u64; 4],
     _marker: PhantomData<F>,
 }
-impl<F: Field + PrimeField> PoseidonConfig<F> {
-    fn configure(
-        meta: &mut ConstraintSystem<F>,
-        path: Column<Advice>,
-        sibling: Column<Advice>,
-        selector: Column<Fixed>,
-    ) -> Self {
-        meta.create_gate("parent value equals to hash of children's values", |meta| {
-            let selector = meta.query_fixed(selector, Rotation::cur());
-            let cur_path = meta.query_advice(path, Rotation::cur());
-            let prev_path = meta.query_advice(path, Rotation::prev());
-            let prev_sibling = meta.query_advice(sibling, Rotation::prev());
-            // TODO: insert the constraints for poseidon hash here
-            vec![cur_path * prev_path * prev_sibling]
+impl<F: Field + PrimeField, const W: usize, const R: usize> PoseidonConfig<F, W, R> {
+    ///
+    pub fn configure(meta: &mut ConstraintSystem<F>) -> Self {
+        assert_eq!(R, W - 1);
+        let state = [0; W].map(|_| meta.advice_column());
+        let sbox = meta.advice_column();
+        let rc_a = [0; W].map(|_| meta.fixed_column());
+        let rc_b = [0; W].map(|_| meta.fixed_column());
+        let s_full = meta.selector();
+        let s_partial = meta.selector();
+        let s_pad_and_add = meta.selector();
+
+        let alpha = [5, 0, 0, 0];
+        let pow_5 = |v: Expression<F>| {
+            let v2 = v.clone() * v.clone();
+            v2.clone() * v2 * v
+        };
+
+        meta.create_gate("partial rounds", |meta| {
+            let cur_0 = meta.query_advice(state[0], Rotation::cur());
+            let mid_0 = meta.query_advice(sbox, Rotation::cur());
+
+            let rc_a0 = meta.query_fixed(rc_a[0], Rotation::cur());
+            let rc_b0 = meta.query_fixed(rc_b[0], Rotation::cur());
+
+            let s_partial = meta.query_selector(s_partial);
+        });
+
+        meta.create_gate("pad-and-add", |meta| {
+            let initial_state_rate = meta.query_advice(state[R], Rotation::prev());
+            let output_state_rate = meta.query_advice(state[R], Rotation::next());
+
+            let s_pad_and_add = meta.query_selector(s_pad_and_add);
+
+            let pad_and_add = |idx: usize| {
+                let initial_state = meta.query_advice(state[idx], Rotation::prev());
+                let input = meta.query_advice(state[idx], Rotation::cur());
+                let output_state = meta.query_advice(state[idx], Rotation::next());
+
+                // We pad the input by storing the required padding in fixed columns and
+                // then constraining the corresponding input columns to be equal to it.
+                initial_state + input - output_state
+            };
+            Constraints::with_selector(
+                s_pad_and_add,
+                (0..R)
+                    .map(pad_and_add)
+                    // The capacity element is never altered by the input.
+                    .chain(Some(initial_state_rate - output_state_rate))
+                    .collect::<Vec<_>>(),
+            )
         });
 
         PoseidonConfig {
-            path,
-            sibling,
+            state,
+            sbox,
+            rc_a,
+            rc_b,
+            s_full,
+            s_partial,
+            s_pad_and_add,
+            alpha,
             _marker: PhantomData,
         }
+    }
+}
+
+/// Trait for a variable in the circuit.
+pub trait Var<F: Field>: Clone + Debug + From<AssignedCell<F, F>> {
+    /// The cell at which this variable was allocated.
+    fn cell(&self) -> Cell;
+
+    /// The value allocated to this variable.
+    fn value(&self) -> Value<F>;
+}
+
+impl<F: Field> Var<F> for AssignedCell<F, F> {
+    fn cell(&self) -> Cell {
+        self.cell()
+    }
+
+    fn value(&self) -> Value<F> {
+        self.value().cloned()
+    }
+}
+
+/// A word in the Poseidon state.
+#[derive(Clone, Debug)]
+pub struct StateWord<F: Field>(AssignedCell<F, F>);
+
+impl<F: Field> From<StateWord<F>> for AssignedCell<F, F> {
+    fn from(state_word: StateWord<F>) -> AssignedCell<F, F> {
+        state_word.0
+    }
+}
+
+impl<F: Field> From<AssignedCell<F, F>> for StateWord<F> {
+    fn from(cell_value: AssignedCell<F, F>) -> StateWord<F> {
+        StateWord(cell_value)
+    }
+}
+
+impl<F: Field> Var<F> for StateWord<F> {
+    fn cell(&self) -> Cell {
+        self.0.cell()
+    }
+
+    fn value(&self) -> Value<F> {
+        self.0.value().cloned()
+    }
+}
+
+#[derive(Debug)]
+struct PoseidonState<F: Field, const W: usize>([StateWord<F>; W]);
+
+impl<F: Field + PrimeField, const W: usize> PoseidonState<F, W> {
+    fn full_round<const R: usize>(
+        self,
+        region: &mut Region<'_, F>,
+        config: &PoseidonConfig<F, W, R>,
+        round: usize,
+        offset: usize,
+    ) -> Result<(), Error> {
+        Ok(())
     }
 }
 
@@ -216,8 +161,6 @@ pub struct MerkleTreeConfig<F: Field + PrimeField> {
     path: Column<Advice>,
     // the sibling nodes of each node in the path
     sibling: Column<Advice>,
-    // the config for checking poseidon constraints
-    poseidon_config: PoseidonConfig<F>,
     // the selectors
     selector: Column<Fixed>,
     selector_root: Selector,
@@ -242,13 +185,12 @@ impl<F: Field + PrimeField> MerkleTreeConfig<F> {
         });
 
         // poseidon constraints
-        let poseidon_config = PoseidonConfig::<F>::configure(meta, path, sibling, selector);
+        // TODO: Write Poseidon constraints
 
         MerkleTreeConfig {
             root,
             path,
             sibling,
-            poseidon_config,
             selector,
             selector_root,
             _marker: PhantomData,
