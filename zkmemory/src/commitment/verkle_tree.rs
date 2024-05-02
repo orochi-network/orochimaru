3373da37608bd5b2248b1a604717e1e78d7cc75c
phamnhatminh1292001 <66765903+phamnhatminh1292001@users.noreply.github.com>
2024-05-02 17:14:02 +0700

    Add Verkle tree config
    Implement Verkle tree commitment scheme and Verkle tree verification circuit.

--- zkmemory/src/commitment/verkle_tree.rs
+++ zkmemory/src/commitment/verkle_tree.rs
@@ -1,1 +1,25 @@
+//! Circuit for proving the correctness of the Verkle tree commitment.
 
+extern crate alloc;
+use core::marker::PhantomData;
+
+use crate::poseidon::poseidon_hash::{ConstantLength, Hash, Spec};
+use alloc::{vec, vec::Vec};
+use ff::{Field, PrimeField};
+use halo2_proofs::{
+    circuit::{Layouter, Region, SimpleFloorPlanner, Value},
+    plonk::{
+        Advice, Circuit, Column, ConstraintSystem, Error, Expression, Fixed, Instance, Selector,
+    },
+    poly::Rotation,
+};
+
+#[derive(Clone, Copy, Debug)]
+/// Verkle tree config
+pub struct VerkleTreeConfig<F: Field + PrimeField> {
+    advice: [Column<Advice>; 2],
+    _marker: PhantomData<F>,
+}
+
+///
+pub(crate) struct VerkleTreeCircuit {}
