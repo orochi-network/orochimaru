fbb79d5941776c5ff557beda48c7cc7723baf340
phamnhatminh1292001 <66765903+phamnhatminh1292001@users.noreply.github.com>
2024-05-06 17:14:16 +0700

    Add proving and verifying merkle tree proof methods

--- zkmemory/src/commitment/verkle_tree.rs
+++ zkmemory/src/commitment/verkle_tree.rs
@@ -2,17 +2,21 @@
 
 extern crate alloc;
 use core::marker::PhantomData;
+extern crate std;
+use std::println;
 
-use crate::poseidon::poseidon_hash::{ConstantLength, Hash, Spec};
+use crate::poseidon::poseidon_hash::{ConstantLength, Hash, OrchardNullifier, Spec};
 use alloc::{vec, vec::Vec};
 use ff::{Field, PrimeField};
 use halo2_proofs::{
+    arithmetic::{eval_polynomial, lagrange_interpolate},
     circuit::{Layouter, Region, SimpleFloorPlanner, Value},
     plonk::{
         Advice, Circuit, Column, ConstraintSystem, Error, Expression, Fixed, Instance, Selector,
     },
     poly::Rotation,
 };
+use halo2curves::pasta::{pallas::Affine, Fp};
 
 #[derive(Clone, Copy, Debug)]
 /// Verkle tree config
@@ -118,5 +122,131 @@ impl<F: Field + PrimeField> Circuit<F> for VerkleTreeCircuit<F> {
         layouter.constrain_instance(leaf_cell.cell(), config.instance, 0)?;
         layouter.constrain_instance(root.cell(), config.instance, 1)?;
         Ok(())
+    }
+}
+
+// verkle tree prove and verify function
+fn hash_commit(commit: &C) -> Fp {
+    let preimage = commit.to_bytes();
+    let preimage_fp = preimage.map(|x| Fp::from(x));
+    Hash::<Fp, OrchardNullifier, ConstantLength<2>, 3, 2>::init().hash(preimage_fp)
+}
+
+fn vec_commit(v: &[Fp], bases: &[Affine]) -> C {
+    unimplemented!()
+}
+
+struct NonLeafNode<const A: usize> {
+    poly_coeffs: Vec<Fp>,
+    commit: C,
+    commit_digest: Fp,
+}
+
+struct VerkleTree<const A: usize> {
+    leafs: Vec<Fp>,
+    non_leaf_layers: Vec<Vec<NonLeafNode<A>>>,
+    root: C,
+}
+
+impl<const A: usize> VerkleTree<A> {
+    fn new(leafs: Vec<Fp>, pedersen_bases: &[Affine]) -> Self {
+        let num_leafs = leafs.len();
+
+        assert!(A.is_power_of_two() && A != 1);
+        assert!(num_leafs.is_power_of_two() && num_leafs >= A);
+        assert_eq!(pedersen_bases.len(), A);
+
+        let log2_arity = A.trailing_zeros() as usize;
+        let log2_leafs = num_leafs.trailing_zeros() as usize;
+        let height = log2_leafs / log2_arity;
+
+        let interp_domain: Vec<Fp> = (0..A as u64).map(Fp::from).collect();
+
+        let mut cur_layer = leafs.clone();
+        let mut non_leaf_layers = Vec::<Vec<NonLeafNode<A>>>::with_capacity(height);
+
+        for _ in 0..height {
+            let next_layer: Vec<NonLeafNode<A>> = cur_layer
+                .chunks(A)
+                .map(|sibs| {
+                    let poly_coeffs = lagrange_interpolate(&interp_domain, sibs);
+                    debug_assert_eq!(poly_coeffs.len(), A);
+                    let commit = vec_commit(&poly_coeffs, pedersen_bases);
+                    let commit_digest = hash_commit(&commit);
+                    NonLeafNode {
+                        poly_coeffs,
+                        commit,
+                        commit_digest,
+                    }
+                })
+                .collect();
+            cur_layer = next_layer.iter().map(|node| node.commit_digest).collect();
+            non_leaf_layers.push(next_layer);
+        }
+
+        debug_assert_eq!(non_leaf_layers.last().unwrap().len(), 1);
+        let root = non_leaf_layers[height - 1][0].commit;
+
+        VerkleTree {
+            leafs,
+            non_leaf_layers,
+            root,
+        }
+    }
+
+    fn root(&self) -> &C {
+        &self.root
+    }
+
+    fn prove(&self, mut challenge: usize) -> VerkleProof<A> {
+        debug_assert!(challenge < self.leafs.len());
+        let leaf = self.leafs[challenge];
+        let polys = self
+            .non_leaf_layers
+            .iter()
+            .map(|layer| {
+                challenge /= A;
+                layer[challenge].poly_coeffs.to_vec()
+            })
+            .collect::<Vec<Vec<Fp>>>();
+        VerkleProof { leaf, polys }
+    }
+}
+
+#[derive(Debug)]
+struct VerkleProof<const A: usize> {
+    leaf: Fp,
+    polys: Vec<Vec<Fp>>,
+}
+
+impl<const A: usize> VerkleProof<A> {
+    fn verify(&self, mut challenge: usize, root: &C, pedersen_bases: &[Affine]) -> bool {
+        let arity_bit_len = A.trailing_zeros() as usize;
+
+        // Check `poly_0(X)` evaluates to provided leaf.
+        let mut height = 0;
+        let mut x = Fp::from((challenge % A) as u64);
+        let mut y = eval_polynomial(&self.polys[0], x);
+        if y != self.leaf {
+            println!("error: poly_{}(x) != leaf", height);
+            return false;
+        }
+        let mut commit = vec_commit(&self.polys[0], pedersen_bases);
+
+        // Check `poly_i(X)` evaluates to the previous polynomial's commitment digest.
+        for poly in &self.polys[1..] {
+            height += 1;
+            let commit_digest = hash_commit(&commit);
+            challenge >>= arity_bit_len;
+            x = Fp::from((challenge % A) as u64);
+            y = eval_polynomial(poly, x);
+            if y != commit_digest {
+                println!("error: poly_{}(x) != commit(poly_{})", height, height - 1);
+                return false;
+            }
+            commit = vec_commit(poly, pedersen_bases);
+        }
+
+        commit == *root
     }
 }
