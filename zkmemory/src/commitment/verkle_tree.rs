8558228ea14b4f4393b3a5457ce95a1960e071cc
phamnhatminh1292001 <66765903+phamnhatminh1292001@users.noreply.github.com>
2024-05-28 17:03:06 +0700

    Add testcases and refactor code

--- zkmemory/src/commitment/verkle_tree.rs
+++ zkmemory/src/commitment/verkle_tree.rs
@@ -2,37 +2,34 @@
 
 extern crate alloc;
 use core::marker::PhantomData;
-use group::Curve;
 extern crate std;
 use crate::{
-    base::B256,
     constraints,
     poseidon::poseidon_hash::{ConstantLength, Hash, Spec},
 };
 use alloc::{vec, vec::Vec};
 use constraints::gadgets::Table;
-use ff::{Field, PrimeField, PrimeFieldBits, WithSmallOrderMulGroup};
+use ff::Field;
 use halo2_proofs::{
-    arithmetic::lagrange_interpolate,
     circuit::{Layouter, Region, SimpleFloorPlanner, Value},
     halo2curves::bn256::{Bn256, Fr, G1Affine},
-    halo2curves::{
-        bn256::{Fq12, G2Affine, Gt},
-        pairing::Engine,
-        CurveAffine,
-    },
+    halo2curves::CurveAffineExt,
     plonk::{
         Advice, Circuit, Column, ConstraintSystem, Error, Expression, Fixed, Instance, Selector,
     },
     poly::{
-        commitment::{Blind, CommitmentScheme, Params, ParamsProver, Prover},
-        kzg::commitment::ParamsKZG,
-        EvaluationDomain, Rotation,
+        commitment::ParamsProver,
+        kzg::{
+            commitment::{KZGCommitmentScheme, ParamsKZG},
+            multiopen::VerifierSHPLONK,
+            strategy::AccumulatorStrategy,
+        },
+        Rotation,
     },
+    transcript::{Blake2bRead, Challenge255},
 };
-use halo2curves::pasta::pallas::{Affine, Scalar};
-use rand_core::OsRng;
-use std::println;
+
+use super::kzg::verify_kzg_proof;
 
 #[derive(Clone, Copy, Debug)]
 /// Verkle tree config
@@ -42,7 +39,8 @@ pub struct VerkleTreeConfig<const A: usize> {
     pub instance: Column<Instance>,
     indices: Column<Advice>,
     selector: Column<Fixed>,
-    selector_zero: Selector,
+    selector_check: Selector,
+    table: Table<A>,
 }
 impl<const A: usize> VerkleTreeConfig<A> {
     fn configure(
@@ -53,7 +51,7 @@ impl<const A: usize> VerkleTreeConfig<A> {
         let advice = [0; 2].map(|_| meta.advice_column());
         let check = meta.advice_column();
         let selector = meta.fixed_column();
-        let selector_zero = meta.selector();
+        let selector_check = meta.selector();
         let indices = meta.advice_column();
         for i in advice {
             meta.enable_equality(i);
@@ -72,8 +70,8 @@ impl<const A: usize> VerkleTreeConfig<A> {
 
         meta.create_gate("verification result should be valid", |meta| {
             let check = meta.query_advice(check, Rotation::cur());
-            let selector = meta.query_fixed(selector, Rotation::cur());
-            vec![selector * (check - Expression::Constant(Fr::ONE))]
+            let selector_check = meta.query_selector(selector_check);
+            vec![selector_check * (check - Expression::Constant(Fr::ONE))]
         });
 
         VerkleTreeConfig {
@@ -82,7 +80,8 @@ impl<const A: usize> VerkleTreeConfig<A> {
             instance,
             indices,
             selector,
-            selector_zero,
+            selector_check,
+            table,
         }
     }
 }
@@ -96,7 +95,7 @@ pub(crate) struct VerkleTreeCircuit<
 > {
     pub(crate) leaf: Fr,
     pub(crate) commitment: Vec<G1Affine>,
-    pub(crate) proof: Vec<G1Affine>,
+    pub(crate) proof: Vec<u8>,
     pub(crate) non_leaf_elements: Vec<Fr>,
     pub(crate) indices: Vec<Fr>,
     pub(crate) params: ParamsKZG<Bn256>,
@@ -112,7 +111,7 @@ impl<S: Spec<Fr, W, R>, const W: usize, const R: usize, const A: usize> Circuit<
         Self {
             leaf: Fr::ZERO,
             commitment: vec![G1Affine::generator()],
-            proof: vec![G1Affine::generator()],
+            proof: vec![0],
             non_leaf_elements: vec![Fr::ZERO],
             indices: vec![Fr::ZERO],
             params: ParamsKZG::<Bn256>::new(1),
@@ -154,7 +153,6 @@ impl<S: Spec<Fr, W, R>, const W: usize, const R: usize, const A: usize> Circuit<
                         self.assign(
                             self.leaf,
                             self.commitment[i],
-                            self.proof[i],
                             self.indices[i],
                             &mut region,
                             config,
@@ -164,7 +162,6 @@ impl<S: Spec<Fr, W, R>, const W: usize, const R: usize, const A: usize> Circuit<
                         self.assign(
                             self.non_leaf_elements[i - 1],
                             self.commitment[i],
-                            self.proof[i],
                             self.indices[i],
                             &mut region,
                             config,
@@ -172,7 +169,42 @@ impl<S: Spec<Fr, W, R>, const W: usize, const R: usize, const A: usize> Circuit<
                         )?;
                     }
                 }
+                config.table.load(&mut region)?;
                 Ok(())
+            },
+        )?;
+
+        let mut poly_list = vec![];
+        poly_list.push(self.leaf);
+        for i in 0..self.non_leaf_elements.len() - 1 {
+            poly_list.push(self.non_leaf_elements[i]);
+        }
+
+        let b = verify_kzg_proof::<
+            KZGCommitmentScheme<Bn256>,
+            VerifierSHPLONK<'_, Bn256>,
+            Challenge255<G1Affine>,
+            Blake2bRead<&'_ [u8], G1Affine, Challenge255<G1Affine>>,
+            AccumulatorStrategy<'_, Bn256>,
+        >(
+            &self.params,
+            self.indices.clone(),
+            poly_list,
+            self.commitment.clone(),
+            self.proof.as_slice(),
+        );
+        let b = Fr::from(b);
+
+        layouter.assign_region(
+            || "assign check result",
+            |mut region| {
+                config.selector_check.enable(&mut region, 0)?;
+                region.assign_advice(
+                    || "assign check result",
+                    config.check,
+                    0,
+                    || Value::known(b),
+                )
             },
         )?;
 
@@ -197,25 +229,10 @@ impl<S: Spec<Fr, W, R>, const W: usize, const R: usize, const A: usize> Circuit<
 impl<S: Spec<Fr, W, R>, const W: usize, const R: usize, const A: usize>
     VerkleTreeCircuit<S, W, R, A>
 {
-    fn vec_commit(child: [Fr; A], omega_power: &[Fr], k: u32, params: ParamsKZG<Bn256>) -> Fr {
-        let domain = EvaluationDomain::new(1, k);
-        let poly = domain.coeff_from_vec(lagrange_interpolate(omega_power, &child));
-        let blind = Blind::<Fr>::new(&mut OsRng);
-        let commit = params.commit(&poly, blind).to_affine();
-        let coordinates = commit.coordinates().unwrap();
-        let x = coordinates.x().to_bytes();
-        let y = coordinates.y().to_bytes();
-        let x_fr = Fr::from(B256::from(x));
-        let y_fr = Fr::from(B256::from(y));
-        let hash = Hash::<Fr, S, ConstantLength<2>, W, R>::init().hash([x_fr, y_fr]);
-        hash
-    }
-
     fn assign(
         &self,
         cur_value: Fr,
         commitment: G1Affine,
-        proof: G1Affine,
         index: Fr,
         region: &mut Region<'_, Fr>,
         config: VerkleTreeConfig<A>,
@@ -227,12 +244,9 @@ impl<S: Spec<Fr, W, R>, const W: usize, const R: usize, const A: usize>
             offset,
             || Value::known(cur_value),
         )?;
-
-        let coordinates = commitment.coordinates().unwrap();
-        let x = coordinates.x().to_bytes();
-        let y = coordinates.y().to_bytes();
-        let x_fr = Fr::from(B256::from(x));
-        let y_fr = Fr::from(B256::from(y));
+        let (x, y) = commitment.into_coordinates();
+        let x_fr = Fr::from_bytes(&x.to_bytes()).unwrap();
+        let y_fr = Fr::from_bytes(&y.to_bytes()).unwrap();
         let next_value = Hash::<Fr, S, ConstantLength<2>, W, R>::init().hash([x_fr, y_fr]);
 
         region.assign_advice(
@@ -249,101 +263,236 @@ impl<S: Spec<Fr, W, R>, const W: usize, const R: usize, const A: usize>
             || Value::known(index),
         )?;
 
-        region.assign_fixed(
-            || "selector",
-            config.selector,
-            offset,
-            || Value::known(Fr::ONE),
-        )?;
+        if offset != 0 {
+            region.assign_fixed(
+                || "selector",
+                config.selector,
+                offset,
+                || Value::known(Fr::ONE),
+            )?;
+        }
 
-        let e1 = Bn256::pairing(&commitment, &G2Affine::generator());
-
-        let g2 = self.params.s_g2() - &self.mul_g2(index);
-        let g2 = halo2_proofs::halo2curves::bn256::G2Affine::from(g2);
-        let e2 = Bn256::pairing(&proof, &g2);
-
-        let g3 = self.mul_g2(cur_value);
-        let e3 = Bn256::pairing(&G1Affine::generator(), &g3);
-
-        let sub = e1 - e2 - e3;
-        let b = Fr::from(sub.eq(&Gt::identity()));
-
-        region.assign_advice(
-            || "result of verification",
-            config.check,
-            offset,
-            || Value::known(b),
-        )?;
-
-        config.selector_zero.enable(region, offset)?;
         Ok(())
     }
-
-    fn mul_g2(&self, scalar: Fr) -> G2Affine {
-        let g2 = G2Affine::generator();
-        let mut sum = G2Affine::generator();
-        let bits = to_bin(scalar.to_bytes());
-        for i in 1..bits.len() {
-            if bits[i] == 0 {
-                sum = (sum.clone() + sum.clone()).into();
-            } else {
-                sum = (sum.clone() + sum.clone() + g2).into();
-            }
-        }
-        sum
-    }
-}
-
-fn to_bin(v: [u8; 32]) -> [u8; 256] {
-    let mut bin = [0; 256];
-    for i in 0..32 {
-        let mut tmp = v[31 - i];
-        for j in 0..8 {
-            bin[8 * (32 - i) - 1 - j] = tmp % 2;
-            tmp = tmp / 2;
-        }
-    }
-    bin
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::poseidon::poseidon_hash::*;
+    use crate::{
+        commitment::kzg::create_kzg_proof,
+        poseidon::{
+            poseidon_constants::{MDS_FR, MDS_INV_FR, ROUND_CONSTANTS_FR},
+            poseidon_hash::*,
+        },
+    };
     use alloc::vec;
     use core::marker::PhantomData;
-    use rand::{thread_rng, Rng};
-    use std::println;
+    use group::Curve;
+    use halo2_proofs::{
+        arithmetic::lagrange_interpolate,
+        dev::MockProver,
+        halo2curves::CurveAffineExt,
+        poly::{
+            commitment::Blind, kzg::multiopen::ProverSHPLONK, Coeff, EvaluationDomain, Polynomial,
+        },
+        transcript::Blake2bWrite,
+    };
+    use rand_core::OsRng;
+    ///
+    pub struct KZGTest {
+        kzg_params: ParamsKZG<Bn256>,
+        domain: EvaluationDomain<Fr>,
+    }
+
+    impl KZGTest {
+        /// Initialize KZG parameters
+        pub fn new(k: u32) -> Self {
+            Self {
+                kzg_params: ParamsKZG::<Bn256>::new(k),
+                domain: EvaluationDomain::new(1, k),
+            }
+        }
+
+        pub fn poly_from_evals(&self, evals: [Fr; 4]) -> Polynomial<Fr, Coeff> {
+            // Use Lagrange interpolation
+            self.domain.coeff_from_vec(lagrange_interpolate(
+                &[Fr::from(0), Fr::from(1), Fr::from(2), Fr::from(3)],
+                &evals,
+            ))
+        }
+
+        pub fn commit(&self, evals: [Fr; 4]) -> G1Affine {
+            self.kzg_params
+                .commit(&self.poly_from_evals(evals), Blind(Fr::random(OsRng)))
+                .to_affine()
+        }
+
+        pub fn create_proof(
+            &self,
+            point: Vec<Fr>,
+            polynomial: Vec<Polynomial<Fr, Coeff>>,
+            commitment: Vec<G1Affine>,
+        ) -> Vec<u8> {
+            create_kzg_proof::<
+                KZGCommitmentScheme<Bn256>,
+                ProverSHPLONK<'_, Bn256>,
+                Challenge255<G1Affine>,
+                Blake2bWrite<Vec<u8>, G1Affine, Challenge255<G1Affine>>,
+            >(&self.kzg_params, point, polynomial, commitment)
+        }
+
+        pub fn group_to_scalar<S: Spec<Fr, W, R>, const W: usize, const R: usize>(
+            &self,
+            g: G1Affine,
+        ) -> Fr {
+            let (x, y) = g.into_coordinates();
+            let x_fr = Fr::from_bytes(&x.to_bytes()).unwrap();
+            let y_fr = Fr::from_bytes(&y.to_bytes()).unwrap();
+            Hash::<Fr, S, ConstantLength<2>, W, R>::init().hash([x_fr, y_fr])
+        }
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct OrchardNullifier;
+
+    impl Spec<Fr, 3, 2> for OrchardNullifier {
+        fn full_rounds() -> usize {
+            8
+        }
+
+        fn partial_rounds() -> usize {
+            56
+        }
+
+        fn sbox(val: Fr) -> Fr {
+            val.pow_vartime([5])
+        }
+
+        fn constants() -> (Vec<[Fr; 3]>, Mtrx<Fr, 3>, Mtrx<Fr, 3>) {
+            (ROUND_CONSTANTS_FR[..].to_vec(), MDS_FR, MDS_INV_FR)
+        }
+    }
+
     #[test]
 
-    fn test_1() {
-        let leaf = Fr::from(0u64);
-        let k = 10;
-        let non_leaf_elements = vec![
-            Fr::from(0u64),
-            Fr::from(0u64),
-            Fr::from(0u64),
-            Fr::from(0u64),
-        ];
-        let params = ParamsKZG::<Bn256>::new(3);
-        let commitment = vec![G1Affine::generator(); 4];
-        let proof = vec![G1Affine::generator(); 4];
-        let indices = vec![
-            Fr::from(0u64),
-            Fr::from(0u64),
-            Fr::from(0u64),
-            Fr::from(0u64),
-        ];
+    fn test_valid_verkle_tree() {
+        let kzg = KZGTest::new(2);
+        let leaf = Fr::from(1);
+        let indices = vec![Fr::from(1)];
+        let evals = [Fr::from(3), leaf, Fr::from(7), Fr::from(15)];
+        let poly0 = vec![kzg.poly_from_evals(evals)];
+        let commit = kzg.commit(evals);
+        let root = kzg.group_to_scalar::<OrchardNullifier, 3, 2>(commit);
+        let non_leaf_elements = vec![root];
+        let commitment = vec![commit];
+        let proof = kzg.create_proof(vec![Fr::from(1)], poly0, commitment.clone());
 
-        let mut rng = thread_rng();
-        let mut chunk = [0u8; 32];
-        for e in chunk.iter_mut() {
-            *e = rng.gen_range(u8::MIN..u8::MAX);
-        }
-        chunk[31] = 0u8;
-        let fr = Fr::from_bytes(&chunk).expect("Unable to convert to Fr");
-        let chunk_fr: [u8; 32] = fr.try_into().expect("Cannot convert from Fr to bytes");
+        let circuit = VerkleTreeCircuit::<OrchardNullifier, 3, 2, 4> {
+            leaf,
+            commitment,
+            proof,
+            non_leaf_elements,
+            indices,
+            params: kzg.kzg_params,
+            _marker: PhantomData,
+        };
 
-        assert_eq!(chunk_fr, chunk);
+        let prover =
+            MockProver::run(10, &circuit, vec![vec![leaf, root]]).expect("Cannot run the circuit");
+        assert_eq!(prover.verify(), Ok(()));
+    }
+
+    #[test]
+    fn wrong_verkle_root() {
+        let kzg = KZGTest::new(2);
+        let leaf = Fr::from(1);
+        let indices = vec![Fr::from(0)];
+        let evals = [leaf, Fr::from(0), Fr::from(0), Fr::from(0)];
+        let poly0 = vec![kzg.poly_from_evals(evals)];
+        let commit = kzg.commit(evals);
+        let root = kzg.group_to_scalar::<OrchardNullifier, 3, 2>(commit);
+        let non_leaf_elements = vec![Fr::from(0)];
+        let commitment = vec![commit];
+        let proof = kzg.create_proof(vec![Fr::from(0)], poly0, commitment.clone());
+
+        let circuit = VerkleTreeCircuit::<OrchardNullifier, 3, 2, 4> {
+            leaf,
+            commitment,
+            proof,
+            non_leaf_elements,
+            indices,
+            params: kzg.kzg_params,
+            _marker: PhantomData,
+        };
+
+        let prover =
+            MockProver::run(10, &circuit, vec![vec![leaf, root]]).expect("Cannot run the circuit");
+        assert_ne!(prover.verify(), Ok(()));
+    }
+
+    #[test]
+    fn wrong_opening_index() {
+        let kzg = KZGTest::new(2);
+        let leaf = Fr::from(1);
+        let indices = vec![Fr::from(0)];
+        let evals = [leaf, Fr::from(0), Fr::from(0), Fr::from(0)];
+        let poly0 = vec![kzg.poly_from_evals(evals)];
+        let commit = kzg.commit(evals);
+        let root = kzg.group_to_scalar::<OrchardNullifier, 3, 2>(commit);
+        let non_leaf_elements = vec![root];
+        let commitment = vec![commit];
+        let proof = kzg.create_proof(vec![Fr::from(1)], poly0, commitment.clone());
+
+        let circuit = VerkleTreeCircuit::<OrchardNullifier, 3, 2, 4> {
+            leaf,
+            commitment,
+            proof,
+            non_leaf_elements,
+            indices,
+            params: kzg.kzg_params,
+            _marker: PhantomData,
+        };
+
+        let prover =
+            MockProver::run(10, &circuit, vec![vec![leaf, root]]).expect("Cannot run the circuit");
+        assert_ne!(prover.verify(), Ok(()));
+    }
+
+    #[test]
+    fn valid_verkle_tree_2() {
+        let kzg = KZGTest::new(2);
+        let leaf = Fr::from(1);
+        let indices = vec![Fr::from(0), Fr::from(1)];
+        let evals = [leaf, Fr::from(1), Fr::from(3), Fr::from(5)];
+        let poly0 = kzg.poly_from_evals(evals);
+        let commit_0 = kzg.commit(evals);
+        let non_leaf_0 = kzg.group_to_scalar::<OrchardNullifier, 3, 2>(commit_0);
+        let evals_1 = [Fr::from(0), non_leaf_0, Fr::from(3), Fr::from(5)];
+        let poly1 = kzg.poly_from_evals(evals_1);
+        let commit_1 = kzg.commit(evals_1);
+        let root = kzg.group_to_scalar::<OrchardNullifier, 3, 2>(commit_1);
+
+        let non_leaf_elements = vec![non_leaf_0, root];
+        let commitment = vec![commit_0, commit_1];
+        let proof = kzg.create_proof(
+            vec![Fr::from(0), Fr::from(1)],
+            vec![poly0, poly1],
+            commitment.clone(),
+        );
+
+        let circuit = VerkleTreeCircuit::<OrchardNullifier, 3, 2, 4> {
+            leaf,
+            commitment,
+            proof,
+            non_leaf_elements,
+            indices,
+            params: kzg.kzg_params,
+            _marker: PhantomData,
+        };
+
+        let prover =
+            MockProver::run(10, &circuit, vec![vec![leaf, root]]).expect("Cannot run the circuit");
+        assert_eq!(prover.verify(), Ok(()));
     }
 }
